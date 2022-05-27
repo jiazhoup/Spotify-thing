8fb091cd07e69e38e51d8832c92d3ef40556fa8c
jiazhoup <pangjiazhou@gmail.com>
2022-05-27 15:55:57 -0700

    main

---
+++ main.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+import os
+
+
+def main(self):
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ spotify.py
@@ -0,0 +1,12 @@
+
+class spotify(object):
+    def __init__(self, api_key):
+        self.api_key = api_key
+
+    def get_tracks(self, song_num=5, location='Los Angeles'):
+        pass
+
+    def
+
+    def add_to_library(self, ids):
+        pass
